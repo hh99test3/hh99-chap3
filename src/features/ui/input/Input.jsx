f4e3997295ff9dea9d6f87d2ecb5b516168ba2c9
Taehyun Kim <agart319@gmail.com>
2023-07-07 12:46:19 +0900

    feat taehyun input기능추가

--- src/features/ui/input/Input.jsx
+++ src/features/ui/input/Input.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { styled } from "styled-components";
 
-export const Input = () => {
-  return <div>Input</div>;
+const InputBox = styled.input`
+  border: 1px solid rgb(51, 51, 51);
+  height: 40px;
+  width: 200px;
+  outline: none;
+  border-radius: 8px;
+  padding-left: 12px;
+  padding-right: 12px;
+`;
+
+export const Input = ({ type, onChange, name, shownValue, isNumber }) => {
+  const [value, setValue] = useState({});
+
+  return (
+    <>
+      <label htmlFor="title">이름변수</label>
+      <InputBox
+        type={type}
+        name={name}
+        value={shownValue}
+        onChange={onChange}
+      ></InputBox>
+    </>
+  );
 };
--- src/features/ui/input/InputContainer.jsx
+++ src/features/ui/input/InputContainer.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "./Input";
+import { styled } from "styled-components";
+
+const Container = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const InputContainer = () => {
+  const [input, setInput] = useState({ name: "", price: "" });
+  const [shownValue, setShownValue] = useState("");
+
+  const onChangePrice = (e) => {
+    console.log(e.nativeEvent.data);
+    // if (e.target.name == "price" && /\d/.test(e.target.data)) {
+    const temp_value = Number(
+      shownValue.concat(e.nativeEvent.data).replace(/[^\d]/g, "")
+    );
+    setInput({ ...input, [e.target.name]: temp_value });
+    setShownValue(temp_value.toLocaleString());
+  };
+
+  const onChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = () => {
+    alert(`name:${input.name}, price:${input.price}`);
+  };
+
+  const num = "123456";
+  console.log(num.toLocaleString());
   return (
     <Container>
-      <Input></Input>
-      <Input></Input>
+      <Input
+        type="text"
+        shownValue={input.name}
+        name="name"
+        onChange={onChange}
+      />
+      <Input
+        type="text"
+        shownValue={shownValue}
+        name="price"
+        onChange={onChangePrice}
+        isNumber
+      />
+      <button onClick={onSubmitHandler}>저장버튼예정지</button>
     </Container>
   );
 };
+// setInput(...input, name: e.target.value.toLocaleString() );
